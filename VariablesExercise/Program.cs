26e60dee187e46cb52e32ddc022c3d3462748cd9
jimklo83 <jimklo83@mail.com>
2023-09-28 11:10:30 -0500

    variables exercise completed

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Words
+            string gangnamStyle;
+            gangnamStyle = "Oppa Gangnam Style";
+            
+            char key;
+            key = 'b';
+
+            bool isAlive;
+            isAlive = true;
+            
+            //Numbers
+            int wholeNum;
+            wholeNum = 5;
+
+            double toilAndTrouble;
+            toilAndTrouble = 4325789.75;
+
+            decimal cashMoney;
+            cashMoney = 14.99m;
+
+            Console.WriteLine($"My favorite song is {gangnamStyle}, I've listened to it {wholeNum} times today and {toilAndTrouble} times throughout my whole life.");
+            Console.WriteLine($"I spent {cashMoney} buying the song.");
+
         }
     }
 }
